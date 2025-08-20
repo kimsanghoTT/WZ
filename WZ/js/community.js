ab721782f7ff53f97a76b98f5f80c1fffd8611f0
kimsangho <qwert8494@naver.com>
2025-08-20 18:27:46 +0900

    커뮤니티페이지 틀 잡기

--- WZ/js/community.js
+++ WZ/js/community.js
@@ -1,10 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper(".mySwiper", {
-        slidesPerView: "4",
+    const swiper = new Swiper(".reels-swiper", {
+        slidesPerView: "4.5",
         spaceBetween: 30,
+        freeMode: {
+            enabled: true,
+            momentumRatio: 0.3, 
+            momentumVelocityRatio: 0.3 
+        },
         scrollbar: {
             el: ".swiper-scrollbar",
-            hide: true,
+            draggable: true,
+            dragSize: 50
         },
     });
+
+    const customProgressBar = document.querySelector('.swiper-custom-progressbar');
+
+    swiper.on('progress', (swiper, progress) => {
+        const progressWidth = progress * 100;
+        customProgressBar.style.width = `${progressWidth}%`;
+    });
+
+});
+
+const popularCheckbox = document.getElementById('popularCheck');
+const popularCheckLabel = document.querySelector('.reels-sort-btn label');
+
+popularCheckbox.addEventListener('change', (e) => {
+  if (e.target.checked) {
+    popularCheckLabel.classList.add("checked");
+  } else {
+    popularCheckLabel.classList.remove("checked");
+  }
 });
