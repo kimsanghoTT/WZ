8ecf17cd5064732d14489e736c0ebdcaf4b64681
tntnrla-lab <tntnrla04@gmail.com>
2025-08-29 17:12:32 +0900

    아코디언 두번째 사진 변경전

--- WZ/main_Accordion/js/Accordion_02.js
+++ WZ/main_Accordion/js/Accordion_02.js
@@ -1,9 +1,9 @@
-const containers = document.querySelectorAll(".accordion-section-01");
+const container = document.querySelector(".accordion-section-02");
 
-containers.forEach(container => {
-    const wrapper = container.querySelector(".accordion-wrapper");
+if (container) {
+    const wrapper = container.querySelector(".accordion-wrapper02");
     const contentBoxes = container.querySelectorAll(".accordion-content-box");
-    const contentText = container.querySelector(".accordion-content-text");
+    const contentText = container.querySelector(".accordion-content-text02");
 
     contentBoxes.forEach(box => {
         box.addEventListener("click", () => {
@@ -14,10 +14,9 @@ containers.forEach(container => {
             if (contentText) {
                 contentText.classList.add("fade-out");
 
-                // 애니메이션 후 완전히 숨김
                 setTimeout(() => {
                     contentText.classList.add("hidden");
-                }, 300); // CSS transition 시간과 맞춰야 함
+                }, 300);
             }
         });
     });
@@ -27,13 +26,9 @@ containers.forEach(container => {
         contentBoxes.forEach(b => b.classList.remove("active"));
 
         if (contentText) {
-            // 다시 보이게
             contentText.classList.remove("hidden");
-
-            // 강제로 리플로우 발생시켜 transition 되게 하기
             void contentText.offsetWidth;
-
             contentText.classList.remove("fade-out");
         }
     });
-});
+}
